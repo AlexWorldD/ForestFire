84cb140e48e125aae87be74b937a07f5a98b94e0
J3FALL <crisp.coad@gmail.com>
2018-01-15 02:04:13 +0300

    Implement state changing for ForestModel

--- model/forest_model.py
+++ model/forest_model.py
@@ -2,19 +2,32 @@ import model.cell as cell
 
 
 class ForestModel:
-    def __init__(self, rows, cols, tree_density):
+    def __init__(self, rows, cols, tree_density, steps):
         self.rows = rows
         self.cols = cols
         self.tree_density = tree_density
         self.init_state = cell.generate_initial_state(rows, cols, tree_density)
 
+        self.steps = steps
         self.current_state = self.init_state
+        self.current_step = 0
+
+        self.current_state = cell.ignite_tree(self.current_state, 2, 2)
+        self.current_state = cell.ignite_tree(self.current_state, 2, 3)
 
     def run(self):
         return True
 
     def reset(self):
-        return True
+        self.current_state = self.init_state
+        self.current_step = 0
+
+    def next(self):
+        if self.current_step < self.steps:
+            self.current_state = cell.get_next_state(self.current_state)
+            self.current_step += 1
+
+        return self.current_state
 
     def stop(self):
         return True
--- test/model/forest_model.py
+++ test/model/forest_model.py
@@ -1,6 +1,19 @@
 import model.forest_model as forest
 
-f = forest.ForestModel(10, 10, 0.7)
+f = forest.ForestModel(10, 10, 1.0, 50)
 
 assert f.current_state == f.init_state
 
+
+def print_cells_matrix(matrix):
+    for row in range(0, len(matrix)):
+        row_str = []
+        for col in range(0, len(matrix[row])):
+            row_str.append(matrix[row][col].state.value)
+        print(row_str)
+
+
+for _ in range(0, 11):
+    print_cells_matrix(f.current_state)
+    print("------------------")
+    f.next()
